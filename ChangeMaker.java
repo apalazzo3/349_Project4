4c997995ccbe0e7582f69f2a12f1e36570ee614d
Michael Lozada <mnmlozada@gmail.com>
2018-02-20 09:21:11 -0800

    skeleton

---
+++ ChangeMaker.java
@@ -0,0 +1,24 @@
+/*
+* CS 349
+* Project 4
+*
+* @authors: Griffin Johnson & Michael Lozada
+* @id: mlozada & gpjohnso
+* @date: February 20, 2018
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class ChangeMaker
+{
+	public static int[] change_DP(int n, int[] d)
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
